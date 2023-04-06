f0176191d7be11f4d6e5e40e5e15122afe094d37
singhyuvraj122 <singhyuvraj122@gmail.com>
2023-04-07 00:44:07 +0530

    add API config in dev environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: "https://openweather43.p.rapidapi.com/weather",
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+  XRapidAPIKeyValue: '4c34fdd50emshe0e4b59b634636ep19f215jsn8c3870e55365',
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'openweather43.p.rapidapi.com'
+};
